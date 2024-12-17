4f85c2ad64bc52c3126675abf73f13726e032af0
Hejwkwkwkk <pulkittravis@gmail.com>
2024-12-18 02:59:03 +0530

    Update RAGNAROK.py

--- RAGNAROK.py
+++ RAGNAROK.py
@@ -15,7 +15,7 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 loop = asyncio.get_event_loop()
 
 # Bot Configuration: Set with Authority
-TOKEN = '7747598023:AAGfi-m_9gw2L_4McP6z62OjGlbc-AavNVY'
+TOKEN = '7629000292:AAF-UcLqU_mTxRdUsOYncN3UFT_Wx3V8YiQ'
 ADMIN_USER_ID = 1724246173
 MONGO_URI = 'mongodb+srv://sharp:sharp@sharpx.x82gx.mongodb.net/?retryWrites=true&w=majority&appName=SharpX'
 USERNAME = "@SharpX72"  # Immutable username for maximum security
